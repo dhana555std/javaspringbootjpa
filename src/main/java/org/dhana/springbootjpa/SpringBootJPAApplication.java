fe5a7d899091d398a00f3cc13163e944b33dd903
dhana555std <dhanapathi.marepalli@gmail.com>
2025-07-30 23:54:33 +0530

    Initial commit

---
+++ src/main/java/org/dhana/springbootjpa/SpringBootJPAApplication.java
@@ -0,0 +1,13 @@
+package org.dhana.springbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"org.dhana.springbootjpa"})
+public class SpringBootJPAApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJPAApplication.class, args);
+	}
+}
