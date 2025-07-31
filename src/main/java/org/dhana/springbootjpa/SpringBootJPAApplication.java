727c62a74f3a09c266e24714797918c35259c74f
dhana555std <dhanapathi.marepalli@gmail.com>
2025-07-31 12:11:47 +0530

    Removing component scan.

--- src/main/java/org/dhana/springbootjpa/SpringBootJPAApplication.java
+++ src/main/java/org/dhana/springbootjpa/SpringBootJPAApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"org.dhana.springbootjpa"})
 public class SpringBootJPAApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJPAApplication.class, args);
